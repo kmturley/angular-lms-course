a1a5682f52d78ada84d92852de489f06211ee740
Kim T <kimturley@gmail.com>
2019-03-13 11:18:01 -0400

    Add child anims, Exclude offline-api from prod

--- src/app/page/page.component.ts
+++ src/app/page/page.component.ts
@@ -1,5 +1,6 @@
 import { ActivatedRoute } from '@angular/router';
-import { Component, OnInit } from '@angular/core';
+import { Component, HostBinding, OnInit } from '@angular/core';
+import { trigger, stagger, animate, style, query, transition } from '@angular/animations';
 
 import { ApiService } from '../shared/api.service';
 
@@ -15,12 +16,30 @@ export class Page {
   type: string;
 }
 
+export const pageTransition = trigger('pageTransition', [
+  transition(':enter', [
+    query('.panel', style({ opacity: 0 }), { optional: true }),
+    query('.panel', stagger(300, [
+      style({ transform: 'translateY(100px)' }),
+      animate('500ms cubic-bezier(.75,-0.48,.26,1.52)', style({transform: 'translateY(0px)', opacity: 1})),
+    ]), { optional: true }),
+  ]),
+  transition(':leave', [
+    query('.panel', stagger(300, [
+      style({ transform: 'translateY(0px)', opacity: 1 }),
+      animate('500ms cubic-bezier(.75,-0.48,.26,1.52)', style({transform: 'translateY(100px)', opacity: 0})),
+    ]), { optional: true }),
+  ])
+]);
+
 @Component({
   selector: 'app-page',
   templateUrl: './page.component.html',
   styleUrls: ['./page.component.scss'],
+  animations: [ pageTransition ],
 })
 export class PageComponent implements OnInit {
+  @HostBinding('@pageTransition') transition = '';
   public page: Page;
 
   constructor(
